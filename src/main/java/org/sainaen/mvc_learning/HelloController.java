27deee7b25145eb44c24e04d738980d069532dc8
sainaen <sainaen@gmail.com>
2013-12-03 18:29:47 +0200

    Update HelloController to support parametrized name

--- src/main/java/org/sainaen/mvc_learning/HelloController.java
+++ src/main/java/org/sainaen/mvc_learning/HelloController.java
@@ -4,13 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
-		return "hello";
-	}
+    private final String messageTemplate = "Hello, %s!";
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model, @RequestParam(defaultValue = "World", required = false) String name) {
+        model.addAttribute("message", String.format(messageTemplate, name));
+        return "hello";
+    }
 }
--- src/test/java/org/sainaen/mvc_learning/AppTests.java
+++ src/test/java/org/sainaen/mvc_learning/AppTests.java
@@ -19,11 +19,10 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @WebAppConfiguration
 @ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
 public class AppTests {
-    private MockMvc mockMvc;
-
     @SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
     protected WebApplicationContext wac;
+    private MockMvc mockMvc;
 
     @Before
     public void setup() {
@@ -32,7 +31,7 @@ public class AppTests {
 
     @Test
     public void simple() throws Exception {
-        mockMvc.perform(get("/"))
+        mockMvc.perform(get("/").param("name", "Ivan"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("hello"));
     }
